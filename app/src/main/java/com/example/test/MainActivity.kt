67faecfc27632e22fa72efebbea1b9ed07a7e4a0
remirhr <remirahir25@gmail.com>
2023-10-18 20:26:07 +0200

    RealFrame229

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.test.frame229.Frame229
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         setContentView(R.layout.activity_main)
     }
